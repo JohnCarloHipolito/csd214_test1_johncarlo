fe13d2bcac2638c2248456284e7d73524bd2ffc4
JohnCarloHipolito <Johncarlo.Hipolito@triosstudent.com>
2024-02-16 13:47:06 -0500

    Identify the Controller File - initial implementation

--- src/main/java/com/triosstudent/csd214_test1_johncarlo/LoginController.java
+++ src/main/java/com/triosstudent/csd214_test1_johncarlo/LoginController.java
@@ -2,13 +2,23 @@ package com.triosstudent.csd214_test1_johncarlo;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 
 public class LoginController {
     @FXML
-    private Label welcomeText;
+    private TextField usernameField;
+    @FXML
+    private PasswordField passwordField;
+    @FXML
+    private Label loginMessage;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    protected void onLoginButtonClick() {
+        String username = usernameField.getText();
+        String password = passwordField.getText();
+
+        loginMessage.setText("");
+        //#TODO: Implement the login logic here
     }
 }
